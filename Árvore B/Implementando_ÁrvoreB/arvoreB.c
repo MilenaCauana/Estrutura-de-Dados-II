b1331cd41c4a5c51b6a8640d7887cabf5fc64aee
Milena Cauana <milenarejani@gmail.com>
2025-09-08 09:43:13 -0300

    Arrumando buscas e adicionando busca Binaria

--- Árvore B/Implementando_ÁrvoreB/arvoreB.c
+++ Árvore B/Implementando_ÁrvoreB/arvoreB.c
@@ -2,57 +2,164 @@
 //Primeiro irei fazer da minha cabeça e, depois, irei conferir com os materiais.
 //Primeiro farei com ponteiros, depois mudo para arquivos binários
 #include<stdlib.h>
+#include<stdio.h>
 
-#typedef MAX 20;
+#define MAX 20
+#define T (MAX + 1)/2 //Aqui, estou definindo t, que será utilizado para fazer a conta do máximo e do mínimo de chaves que um nó deve ter
 
 // ---- DEFININDO A ESTRUTURA DO NÓ ----
 typedef struct No
 {
-    int n; //Número de chaves
-    int folha; //Booleano
-    int chave [MAX]; //Aqui, estou criando o vetor que armazenará os dados
-    struct No *filhos[MAX + 1]; //Ponteiros para os filhos
+    int numeroDeChaves;
+    int ehFolha;
+    int chave[MAX];
+    struct No *filho[MAX + 1];
 
-}NoArvB, *raizArvB;
+}NoArvB, *ArvoreB;
 
-//Função de busca para a chave
-/*
-* Utilizarei recursão:
-* - Parará quando achar;
-* - Parará quando chegar em uma folha e não encontrar.
-*/
-int buscaArvB(raizArvB no, int busca)
+//Criando uma estrutura que irá me permitir retornar o nó e o índice na função de busca
+typedef struct
 {
-    int i;
+    int indiceChave;
+    ArvoreB noEncotrado;
+} RetornoBusca;
 
-    for (i = 0; i < no.n; i++)
+//Função de busca sem busca Binária np vetor de chaves
+RetornoBusca buscaArvB(ArvoreB raiz, int chaveBusca)
+{
+    int i = 0;
+
+    while(i < raiz->numeroDeChaves && chaveBusca > raiz -> chave[i])
     {
-        if (no.chave[i] == busca)
+        i++;
+    }
+
+    if (i < raiz -> numeroDeChaves && chaveBusca == raiz -> chave[i])
+    {
+        RetornoBusca noEncontrado;
+        noEncontrado.indiceChave = i;
+        noEncontrado.noEncotrado = raiz;
+
+        return (noEncontrado);
+    }
+
+    if (raiz -> ehFolha)
+    {
+        return NULL;
+    }
+
+    return buscaArvB(raiz -> filho[i]);
+}
+
+//Função de busca com busca Binária np vetor de chaves
+RetornoBusca buscaArvBBuscaBinaria(ArvoreB raiz, int chaveBusca)
+{
+    int i = raiz -> numeroDeChaves/2;
+    int indiceMaximo = raiz -> numeroDeChaves;
+    int indiceMinimo = 0;
+
+    while (indiceMaximo != indiceMinimo)
+    {
+        if(chaveBusca == raiz -> chave[i])
         {
-            return no.chave[i];
+            RetornoBusca noEncontrado;
+            noEncontrado.indiceChave = i;
+            noEncontrado.noEncotrado = raiz;
+
+            return (noEncontrado);
         }
-        if (busca < no.chave[i])
+
+        if(chaveBusca < raiz -> chave[i])
         {
-            buscarArvB(no.filhos[i], busca);
+            indiceMaximo = i;
+            i = (i - indiceMinimo) /2;
+        } else if(chaveBusca > raiz -> chave[i])
+        {
+            indiceMinimo = i;
+            i = ((indiceMaximo - i) / 2) + i;
         }
     }
 
-    if (no.folha)
+    return NULL;
+}
+
+//Split ou Divisão
+/* Seja um nó cheio, crie dois novos nós:
+* - Um com as chaves [1], ..., chave[t-1] e outro com as chaves chave[t+a],...,chave[t].
+*/
+void splitArvB(raizArvB noMae, raizArvB noCheio)
+{
+
+    int meio = noCheio -> n/2;
+    int chaveSubida = noCheio -> chave[meio];
+
+    //Criar os novos nós
+    raizArvB novo_no1 = (*NoArvB) malloc(sizeof(NoArvB));
+    raizArvB novo_no2 = (*NoArvB) malloc(sizeof(NoArvB));
+
+    //Colocando as informações prévias dos nós
+    novo_no1 -> n = meio;
+    novo_no2 -> n = noCheio -> n - (meio + 1);
+    novo_no1 -> folha = noCheio -> folha;
+    novo_no2 -> folha = noCheio -> folha;
+
+    for(j = 0; j < no.chave[meio]; j++)
+        {
+            novo_no1.chave[j] = noCheio.chave[j];
+            novo_no1.filhos[j] = noCheio.filhos[j];
+        }
+    novo_no1 -> filhos[meio] = noCheio -> filhos[meio];
+
+    // Copia as chaves e filhos para o segundo novo nó
+    for (int i = 0; i < novoNo2->n; i++)
     {
-        printf("\nValor nao encontrado");
-        return -1;
+        novo_no2->chave[i] = noCheio->chave[meio + 1 + i];
+        novo_no2->filhos[i] = noCheio->filhos[meio + 1 + i];
+    }
+    novo_no2->filhos[novo_no2->n] = noCheio->filhos[noCheio->n];
+
+    //Agora, irei colocar o elemento no no mãe e mover seus elementos a direita
+    for(i = 0; i <= noMae -> n; i++)
+    {
+        int aux;
+        if (noMae -> chave[i] == noMae -> chave[noMae -> n]) //Se o local onde o elemento for inserido for o vago depois do n-1
+        {
+            noMae -> chave[i] = chaveSubida;
+            noMae -> filhos[i] = novo_no1;
+            noMae -> filhos[i + 1] = novo_no2;
+            break;
+        } else if(chaveSubida < noMae -> chave[i]) //Se ele encontrar um lugar no meio do vetor
+        {
+            aux = noMae -> chave[i-1];
+            noMae -> chave[i-1] = aux;
+
+            raizArvB no_aux = noMae -> filhos[i];
+            noMae -> filhos[i - 1] = novo_no1;
+            noMae -> filhos[i] = novo_no2;
+
+            int aux1;
+            for (j = i; j <= noMae-> n; j++)
+            {
+                aux1 = noMae -> chave[i];
+                noMae -> chave[i] = aux;
+                aux = aux1;
+
+                raizArvB no_aux1 = noMae -> filhos[i + 1];
+                noMae -> filhos[i + 1] = no_aux;
+                no_aux = no_aux1;
+            }
+        }
     }
 
-    buscaArvB(no.filhos[i], busca);
+    //Agora, veremos se o nó mãe está cheio, para ir subindo recursivamente, se não, ele retorna.
 }
 
 //Função de busca para o nó (Inserção)
 /*
 * Utilizarei recursão:
-* - Parará quando achar;
-* - Parará quando chegar em uma folha e não encontrar.
+* - Parará quando achar a folha em que o número será inserida;
 */
-raizArvB buscaArvB(raizArvB no, int busca)
+raizArvB buscaArvB(raizArvB noMae, raizArvB no, int busca)
 {
     int i;
 
@@ -61,20 +168,26 @@ raizArvB buscaArvB(raizArvB no, int busca)
         if (no.chave[i] == busca)
         {
             printf("\nElemento ja existe!");
-            return -1;
+            return NULL;
         }
 
         if (busca < no.chave[i])
         {
             if (no.folha)
             {
-                return no;
+                if (no.n > MAX)
+                {
+                    //Chama a função split;
+                } else
+                {
+                    return no;
+                }
             }
-            buscarArvB(no.filhos[i], busca);
+            buscarArvB(no, no.filhos[i], busca);
         }
     }
 
-    buscaArvB(no.filhos[i], busca);
+    buscaArvB(no, no.filhos[i], busca);
 }
 
 
